9b888363e98fa7d23742e1fe22e174b5dbec03f2
TannerMillican <tannermillican@gmail.com>
2020-04-25 20:16:19 -0500

    made sure to route to home.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -60,4 +60,8 @@ module.exports = function(app) {
         console.log(res)
     });
 
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
 }
