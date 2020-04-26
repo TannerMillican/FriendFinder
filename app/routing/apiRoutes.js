9170375bd39f346a036a9cad88ff491a2e45a02f
TannerMillican <tannermillican@gmail.com>
2020-04-25 20:10:20 -0500

    got server logic to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,56 @@ module.exports = function(app) {
         res.json(friendsData)
     });
 
+
     app.post('/api/friends', function(req, res){
-        friendsData.push(req.body)
+        // console.log(req)
+        // console.log(res)
+
+        var differencesArr = [];
+        var compatableFriend
+
+        for (var i = 0; i < friendsData.length; i++){
+
+            var possibleFriendsScoreArr = friendsData[i].scores;
+
+            var userScoreArr = req.body.scores;
+
+            var totalDifference = 0;
+
+            var differences = possibleFriendsScoreArr.map(function(item, index){
+                return item - userScoreArr[index]
+            })
+            for (var j = 0; j < differences.length; j++){
+                totalDifference += differences[j]
+            }
+            differencesArr.push(totalDifference)
+        }
+
+        console.log(differencesArr)
+
+        var minDifference = Math.min(...differencesArr)
+        console.log(minDifference)
+
+        var differenceIndex;
+
+        for (var i = 0; i < differencesArr.length; i++){
+            if (minDifference === differencesArr[i]){
+                differenceIndex = differencesArr.indexOf(minDifference)
+            }
+        }
+
+
+        // console.log(compatableFriend)
+
+        for (var i = 0; i < friendsData.length; i++){
+
+            var friendIndex = friendsData[differenceIndex];
+            
+            compatableFriend = friendIndex
+
+        }
+        res = compatableFriend
+        console.log(res)
     });
 
 }
